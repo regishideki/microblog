1206f3bec492bdb6aefe3a42540192f9f73c429b
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2015-02-01 23:00:24 +0000

    change migration create_post name

--- db/migrate/20150130181937_create_posts.rb
+++ db/migrate/20150130181937_create_posts.rb
@@ -1,7 +1,9 @@
-class AddIndexToSlugTitle < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration
   
   def change
     create_table :posts do |t|
+      t.string   "title",      null: false
+      t.text     "message",    null: false
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
       t.string   "slug_title", null: false
